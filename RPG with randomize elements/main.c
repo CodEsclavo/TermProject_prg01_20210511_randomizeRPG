b398f35b3ad328a9c2725ef76c8e764e696aa47b
CodEsclavo <90739141+CodEsclavo@users.noreply.github.com>
2021-11-04 20:51:35 +0900

    10-04

--- RPG with randomize elements/main.c
+++ RPG with randomize elements/main.c
@@ -24,8 +24,7 @@
 
 char CELL_GAME[HEIGHT_GAME][WIDTH_GAME] = { ' ' };
 
-void gotoxy(int, int);
-
+//좌표 지정
 void gotoxy(int x, int y) {
 	COORD pos = { x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
@@ -81,26 +80,33 @@ void cell_deployment() {
 	}
 }
 
+// 셀 정리
+void clear_cell() {
+	for (int i = 1; i < 23; i++) {
+		for (int k = 1; k < 127; k++) {
+			printf(" ");
+		}
+	}
+}
+
 //직업분류
 //1:거지,2:성직자,3:목수,4:개장수,5:기사,6:농부
-int class_rating[6] = { 1,2,3,4,5,6 };
 int player_class;
 
 //1.힘 2.화술 3.지구력 4.지능 5.행운
-int player_stats[5] = { 1,1,1,1,1 };
-
+int player_stats[6] = { 0 };
 
 struct main_character {
 	char name[17];
 	int LVL;
-	int class;
-	int stats[5];
+	char class[20];
+	int stats[6];
 	int HP;
 	int money;
 } player;
 
 //개체 프리셋
-typedef struct object_build {
+typedef struct {
 	char name[50];
 	int LVL;
 	int stats[5];
@@ -109,33 +115,38 @@ typedef struct object_build {
 } NPC;
 
 //무기 프리셋
-struct item_wep {
+typedef struct {
 	char name[50];
 	int atk;
 	int durability;
 	int value;
-};
+} wep;
 
 //방어구 프리셋
-struct item_armr {
+typedef struct {
 	char name[50];
 	int def;
 	int value;
-};
+} armr;
 
 //의약품 프리셋
-struct item_meds {
+typedef struct {
 	char name[50];
 	int heal_amount;
 	int value;
-};
+} meds;
 
-struct {
+struct inventory {
 	int Weapons[10];
 	int Armours[10];
 	int Meds[10];
-} player_inventory;
-
+} Inv;
+/*
+struct equipments {
+	wep;
+	armr;
+} equip;
+*/
 void cell_deployment();
 
 int DICEROLL(int);
@@ -144,14 +155,15 @@ void check_N_confirm();
 int yes_or_no();
 
 int LVL_scailing(char[], int);
-void creat_monster(int);
-struct item_wep weapons(int);
-struct item_armr armours(int);
-struct item_meds medications(int);
+NPC creat_monster(int);
+wep weapons(int);
+armr armours(int);
+meds medications(int);
 
-int* stat_distribution(int[], int, char[]);
-const char* setting_name();
-int setting_class(char[]);
+int *stat_distribution(int[], int, char[]);
+char* set_name();
+char *set_class(char[]);
+struct main_character *initialize();
 
 void visit_shop();
 void meet_monster();
@@ -162,12 +174,13 @@ void earn_item();
 void approach();
 void hit_the_road();
 void battle();
-void display(int, int);
 
+int next_behave();
+void display(struct main_character);
 
 //주사위 굴리기
 int DICEROLL(int side) {
-	int result = rand() % side;
+	int result = rand() % (side - 1) + 1;
 	return result;
 }
 
@@ -201,7 +214,6 @@ void check_N_confirm() {
 		printf(".");
 	}
 
-	system("cls");
 }
 
 int yes_or_no() {
@@ -260,8 +272,10 @@ int LVL_scailing(char index[20], int player_LVL) {
 }
 
 //몬스터 종류 선언, 랜덤생성
-void creat_monster(int player_level) {
+NPC creat_monster(int player_level) {
 
+	int num = rand() % 4;
+	
 	NPC ghoul = {
 		"구울",
 		LVL_scailing("LVL", player_level),
@@ -301,16 +315,40 @@ void creat_monster(int player_level) {
 		LVL_scailing("HP", player_level),
 		LVL_scailing("money", player_level)
 	};
+
+	switch (num) {
+
+	case 0:
+		return ghoul;
+		break;
+
+	case 1:
+		return raider;
+		break;
+
+	case 2:
+		return radsquito;
+		break;
+
+	case 3:
+		return roach;
+		break;
+
+	case 4:
+		return subject;
+		break;
+	}
+
 }
 
 
 // 아이템
-struct item_wep weapons(int wepnum) {
+wep weapons(int wepnum) {
 
 	//무기류
 	if (wepnum == 0) {
 
-		struct item_wep knife = {
+		wep knife = {
 		"칼",
 		rand() % 10 + 5,
 		rand() % 10 + 10,
@@ -322,7 +360,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 1) {
 
-		struct item_wep bat = {
+		wep bat = {
 		"야구배트",
 		rand() % 13 + 12,
 		rand() % 12 + 15,
@@ -334,7 +372,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 2) {
 
-		struct item_wep g_club = {
+		wep g_club = {
 		"골프채",
 		rand() % 10 + 15,
 		rand() % 13 + 10,
@@ -346,7 +384,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 3) {
 
-		struct item_wep imprv_firearm = {
+		wep imprv_firearm = {
 		"급조 총기",
 		rand() % 10 + 25,
 		rand() % 5 + 10,
@@ -358,7 +396,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 4) {
 
-		struct item_wep revolver = {
+		wep revolver = {
 		"리볼버",
 		rand() % 20 + 30,
 		rand() % 10 + 20,
@@ -370,7 +408,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 5) {
 
-		struct item_wep shotgun = {
+		wep shotgun = {
 		"산탄총",
 		rand() % 50 + 50,
 		rand() % 5 + 15,
@@ -382,7 +420,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 6) {
 
-		struct item_wep hunting_rifle = {
+		wep hunting_rifle = {
 		"사냥용 소총",
 		rand() % 20 + 70,
 		rand() % 20 + 25,
@@ -394,7 +432,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 7) {
 
-		struct item_wep assault_rifle = {
+		wep assault_rifle = {
 		"돌격소총",
 		rand() % 40 + 100,
 		rand() % 10 + 40,
@@ -406,7 +444,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else if (wepnum == 8) {
 
-		struct item_wep railgun = {
+		wep railgun = {
 		"레일건",
 		rand() % 50 + 200,
 		rand() % 10 + 20,
@@ -418,7 +456,7 @@ struct item_wep weapons(int wepnum) {
 	
 	else {
 
-		struct item_wep fat_boy = {
+		wep fat_boy = {
 			"팻 보이",
 			99999,
 			1,
@@ -429,13 +467,13 @@ struct item_wep weapons(int wepnum) {
 	}
 }
 
-struct item_armr armours(int defnum) {
+armr armours(int defnum) {
 
 	//방어구류
 
 	if (defnum == 0) {
 
-		struct item_armr rags = {
+		armr rags = {
 		"누더기",
 		rand() % 10 + 10,
 		30 + (int)rags.def * 0.8
@@ -446,7 +484,7 @@ struct item_armr armours(int defnum) {
 	
 	else if (defnum == 1) {
 
-		struct item_armr light_armour = {
+		armr light_armour = {
 		"가벼운 방어구",
 		rand() % 5 + 15,
 		55 + (int)light_armour.def * 0.8
@@ -457,7 +495,7 @@ struct item_armr armours(int defnum) {
 	
 	else if (defnum == 2) {
 
-		struct item_armr stabprf_vest = {
+		armr stabprf_vest = {
 		"방검복",
 		rand() % 20 + 35,
 		70 + (int)stabprf_vest.def * 0.75
@@ -468,7 +506,7 @@ struct item_armr armours(int defnum) {
 	
 	else if (defnum == 3) {
 
-		struct item_armr bulletprf_vest = {
+		armr bulletprf_vest = {
 		"방탄조끼",
 		rand() % 20 + 50,
 		120 + (int)bulletprf_vest.def * 0.6
@@ -479,7 +517,7 @@ struct item_armr armours(int defnum) {
 	
 	else if (defnum == 4) {
 
-		struct item_armr exo_skel = {
+		armr exo_skel = {
 				"엑소스켈레톤",
 				500,
 				2000
@@ -490,7 +528,7 @@ struct item_armr armours(int defnum) {
 	
 	else {
 
-		struct item_armr goliath = {
+		armr goliath = {
 				"\"골리앗\" 신체강화 수트",
 				9999,
 				99999
@@ -500,13 +538,13 @@ struct item_armr armours(int defnum) {
 	}
 }
 
-struct item_meds medications(int mednum) {
+meds medications(int mednum) {
 
 	//의약품류
 
 	if (mednum == 0) {
 
-		struct item_meds enrgbar = {
+		meds enrgbar = {
 		"에너지바",
 		5,
 		30
@@ -517,7 +555,7 @@ struct item_meds medications(int mednum) {
 
 	else if (mednum == 1) {
 
-		struct item_meds bandage = {
+		meds bandage = {
 		"붕대",
 		15,
 		75
@@ -528,7 +566,7 @@ struct item_meds medications(int mednum) {
 
 	else if (mednum == 2) {
 
-		struct item_meds painkiller = {
+		meds painkiller = {
 		"진통제",
 		17.5,
 		85
@@ -539,7 +577,7 @@ struct item_meds medications(int mednum) {
 
 	else if (mednum == 3) {
 
-		struct item_meds first_aid = {
+		meds first_aid = {
 		"구급상자",
 		35,
 		155
@@ -549,7 +587,7 @@ struct item_meds medications(int mednum) {
 	}
 
 	else {
-		struct item_meds survival_kit = {
+		meds survival_kit = {
 		"서바이벌 키트",
 		55,
 		275
@@ -562,9 +600,11 @@ struct item_meds medications(int mednum) {
 
 
 //스탯 분배
-int* stat_distribution(int top_stats[5], int point_remain, char player_name[17]) {
+int *stat_distribution(int temp_stats[6], int point_remain, char player_name[17]) {
 
 	printf("\n투자할 스탯과 수치를 입력하세요. (point remain : %d)\n(1:힘 2:화술 3:지구력 4:지능 5:행운)\n( ex) 1 2 : 힘 + 2 )\n", point_remain);
+
+	*temp_stats = malloc(sizeof(int) * 6);
 
 	int stat_points = point_remain;
 	int invested_point = 0;
@@ -593,18 +633,18 @@ int* stat_distribution(int top_stats[5], int point_remain, char player_name[17])
 			}
 		}
 
-		top_stats[select_stat - 1] += used_point;
+		temp_stats[select_stat] += used_point;
 		invested_point += used_point;
 		printf("\n사용한 포인트 : %d, 잔여 포인트 : %d\n", invested_point, stat_points - invested_point);
 	}
 
-	printf("\n분배 완료!\n\n%s의 능력치\n|| 힘 : %d || 화술 : %d || 지구력 : %d || 지능 : %d || 행운 : %d ||\n", player_name, top_stats[0], top_stats[1], top_stats[2], top_stats[3], top_stats[4]);
+	printf("\n분배 완료!\n\n%s의 능력치\n|| 힘 : %d || 화술 : %d || 지구력 : %d || 지능 : %d || 행운 : %d ||\n", player_name, temp_stats[1], temp_stats[2], temp_stats[3], temp_stats[4], temp_stats[5]);
 
-	return top_stats;
+	return temp_stats;
 }
 
 //게임을 시작하고 캐릭터 생성
-const char* setting_name() {
+char* set_name() {
 
 	static char player_name[17] = "\0";
 
@@ -615,11 +655,15 @@ const char* setting_name() {
 	return player_name;
 }
 
-int setting_class(char player_name[17]) {
+char* set_class(char player_name[17]) {
 
 	printf("\n%s의 직업을 정하기 위해 주사위를 굴립니다.\n\n", player_name);
 
-	player_class = class_rating[DICEROLL(6)];
+	player_class = DICEROLL(6);
+
+	static char array[20] = "\0";
+
+	int* a = &array;
 
 	DICE_rolling();
 
@@ -627,41 +671,221 @@ int setting_class(char player_name[17]) {
 	case 1:
 		printf("주사위 %d(이)가 나왔습니다.\n\n", player_class);
 		printf("%s의 직업은 거지 입니다.\n", player_name);
+		a = "거지";
 		break;
 	case 2:
 		printf("주사위 %d(이)가 나왔습니다.\n\n", player_class);
 		printf("%s의 직업은 성직자 입니다.\n", player_name);
+		a = "성직자";
 		break;
 	case 3:
 		printf("주사위 %d(이)가 나왔습니다.\n\n", player_class);
 		printf("%s의 직업은 목수 입니다.\n", player_name);
+		a = "목수";
 		break;
 	case 4:
 		printf("주사위 %d(이)가 나왔습니다.\n\n", player_class);
 		printf("%s의 직업은 개장수 입니다.\n", player_name);
+		a = "개장수";
 		break;
 	case 5:
 		printf("주사위 %d(이)가 나왔습니다.\n\n", player_class);
 		printf("%s의 직업은 기사 입니다.\n", player_name);
+		a = "기사";
 		break;
 	case 6:
 		printf("주사위 %d(이)가 나왔습니다.\n\n", player_class);
 		printf("%s의 직업은 농부 입니다.\n", player_name);
+		a = "농부";
 		break;
 	}
 
-	return player_class;
+	return array;
+}
+
+struct main_character* initialize() {
+
+	struct main_character* temp = malloc(sizeof(struct main_character));
+
+	strcpy_s(temp->name, 17, set_name());
+
+	strcpy_s(temp->class, 20, set_class(temp->name));
+
+	check_N_confirm();
+
+	system("cls");
+
+	printf("###초기 스탯을 설정합니다.###\n");
+
+	for (int i = 0; i < 6; i++) {
+		player_stats[i] += 1;
+	}
+
+	int *temp_stats = stat_distribution(player_stats, 10, temp->name);
+
+	for (int i = 0; i < 6; i++) {
+		temp->stats[i] = temp_stats[i];
+	}
+
+	check_N_confirm();
+
+	temp->LVL = 1;
+
+	temp->HP = (50 + temp->LVL * 10 + temp->stats[3] * 5);
+
+	temp->money = 100;
+
+	return temp;
 }
 
 
-struct main_character initialize () {
+void approach() {
 
+	int encounter_location = rand() % 5 + 1;
+	int choice;
+
+	switch (encounter_location) {
+	case 1:
+		gotoxy(1, 1); printf("앞에 웬 판잣집이 있다.");
+		printf("안에서 기척이 느껴진다");
+		printf("다가가 보시겠습니까?");
+
+		choice = yes_or_no();
+
+		if (choice == 1) {
+
+
+		}
+
+		else {
+			printf("무엇을 하시겠습니까?");
+
+			printf("무언가 수상한 판잣집을 뒤로한채 발을 옮겼다.");
+
+		}
+
+		break;
+	case 2:
+		gotoxy(1, 1); printf("앞에 웬 폐건물이 있다.");
+		printf("들어가 보시겠습니까?");
+
+		choice = yes_or_no();
+
+		if (choice == 1) {
+			printf("가까이 다가갔더니 인기척이 느껴졌다.");
+
+		}
+
+		else {
+			printf("무엇을 하시겠습니까?");
+
+			printf("무언가 수상한 폐건물을 뒤로한채 발을 옮겼다.");
+
+		}
+
+		break;
+	case 3:
+		gotoxy(1, 1); printf("앞에 웬 멀쩡한 건물이/가 있다.");
+		printf("들어가 보시겠습니까?");
+
+		choice = yes_or_no();
+
+		if (choice == 1) {
+			printf("가까이 다가갔더니 인기척이 느껴졌다.");
+
+		}
+
+		else {
+			printf("무엇을 하시겠습니까?");
+
+			printf("무언가 수상한 멀쩡한 건물을 뒤로한채 발을 옮겼다.");
+
+		}
+
+		break;
+	case 4:
+		gotoxy(1, 1);	printf("앞에 웬 움막이 있다.");
+		printf("안에서 기척이 느껴진다");
+		printf("다가가 보시겠습니까?");
+
+		choice = yes_or_no();
+
+		if (choice == 1) {
+			printf("가까이 다가갔더니 인기척이 느껴졌다.");
+
+		}
+
+		else {
+			printf("무엇을 하시겠습니까?");
+
+			printf("무언가 수상한 움막을 뒤로한채 발을 옮겼다.");
+
+		}
+
+		break;
+	case 5:
+		gotoxy(1, 1); printf("앞에 웬 벙커가 있다.");
+		printf("들어가 보시겠습니까?");
+
+		choice = yes_or_no();
+
+		if (choice == 1) {
+			printf("가까이 다가갔더니 인기척이 느껴졌다.");
+
+		}
+
+		else {
+			printf("무엇을 하시겠습니까?");
+
+			printf("무언가 수상한 벙커를 뒤로한채 발을 옮겼다.");
+
+		}
+
+		break;
+	case 6:
+		gotoxy(1, 1); printf("앞 길거리에 무언가 있다.");
+
+		break;
+	}
 }
 
+void hit_the_road() {
+
+	int rslt = DICEROLL(2);
+
+	if (rslt == 1) {
+		gotoxy(1, 21); printf("갈길 가는 중.");
+		for (int i = 0; i < 7; i++) {
+			Sleep(250);
+			printf(".");
+		}
+	}
+
+	else {
+		gotoxy(1, 21); printf("가던 길 마저 가는중.");
+		for (int i = 0; i < 7; i++) {
+			Sleep(250);
+			printf(".");
+		}
+	}
+
+	for (int i = 0; i < 6; i++) {
+		printf(".");
+		Sleep(300);
+	}
+
+	clear_cell();
+} 
+
+void battle(struct main_character temp) {
+
+	NPC monster = creat_monster(temp.LVL);
+
+}
 
 //인카운터 설정
 void visit_shop() {
-	printf("선반에 여러가지 물건이 진열되어있다, 상점인 듯하다\n");
+	gotoxy(10, 1); printf("선반에 여러가지 물건이 진열되어있다, 상점인 듯하다\n");
 	
 }
 
@@ -682,127 +906,98 @@ void earn_item() {
 
 }
 
+int next_behave() {
 
+	int X;
 
-void approach() {
+	gotoxy(10, 1); printf("무얼 하시겠습니까?\n");
+	gotoxy(10, 3); printf("1. 갈 길 가기");
+	gotoxy(10, 4); printf("2. 소지품 확인");
+	gotoxy(10, 5); printf("3. 휴식하기");
+	gotoxy(10, 6); printf("4. 종료하기");
 
-	int encounter_location = rand() % 4 + 1;
-	int choice;
+	gotoxy(10, 8); scanf_s("%d", &X);
 
-	switch (encounter_location) {
-	case 1:
-		printf("\n앞에 웬 판잣집이 있다.\n");
-		printf("안에서 기척이 느껴진다\n");
-		printf("다가가 보시겠습니까?\n");
+	while (X != 1 && X != 2 && X != 3 && X != 4) {
 
-		choice = yes_or_no();
-
-		if (choice == 1) {
-			
-
+		for (int i = 0; i < 8; i++) {
+			for (int k = 1; k < 50; k++) {
+					gotoxy(k, i); printf(" ");
+			}
 		}
 
-		else {
-			printf("무엇을 하시겠습니까?\n");
+		gotoxy(10, 1); printf("!!ERROR!! 다시 입력하세요");
+		Sleep(3000);
 
-			printf("무언가 수상한 판잣집을 뒤로한채 발을 옮겼다.\n");
-
+		for (int k = 1; k < 50; k++) {
+			gotoxy(k, 1); printf(" ");
 		}
 
+		gotoxy(10, 1); printf("무얼 하시겠습니까?\n");
+		gotoxy(10, 3); printf("1. 갈 길 가기");
+		gotoxy(10, 4); printf("2. 소지품 확인");
+		gotoxy(10, 5); printf("3. 휴식하기");
+		gotoxy(10, 6); printf("4. 종료하기");
+
+		gotoxy(10, 8); scanf_s("%d", &X);
+		
+	}
+
+	
+
+	for (int i = 1; i < 8; i++) {
+		for (int k = 1; k < 50; k++) {
+			gotoxy(k, i); printf(" ");
+		}
+	}
+
+	return X;
+
+}
+
+void player_action() {
+
+	int temp = next_behave();
+
+	switch (temp) {
+	case 1: 
+		hit_the_road();
+		approach();
 		break;
+
 	case 2:
-		printf("\n앞에 웬 폐건물이 있다.\n");
-		printf("들어가 보시겠습니까?\n");
-
-		choice = yes_or_no();
-
-		if (choice == 1) {
-			printf("가까이 다가갔더니 인기척이 느껴졌다.\n");
-
-		}
-
-		else {
-			printf("무엇을 하시겠습니까?\n");
-
-			printf("무언가 수상한 폐건물을 뒤로한채 발을 옮겼다.\n");
-
-		}
 
 		break;
+
 	case 3:
-		printf("\n앞에 웬 멀쩡한 건물이/가 있다.\n");
-		printf("들어가 보시겠습니까?\n");
-
-		choice = yes_or_no();
-
-		if (choice == 1) {
-			printf("가까이 다가갔더니 인기척이 느껴졌다.\n");
-
+		gotoxy(10, 1); printf("휴식중.");
+		for (int i = 0; i < 7; i++) {
+			Sleep(250);
+			printf(".");
 		}
-
-		else {
-			printf("무엇을 하시겠습니까?\n");
-
-			printf("무언가 수상한 멀쩡한 건물을 뒤로한채 발을 옮겼다.\n");
-
-		}
-
+		printf("50의 체력을 회복했습니다.");
+		player.HP += 50;
+		check_N_confirm();
+		clear_cell();
 		break;
+
 	case 4:
-		printf("\n앞에 웬 움막이 있다.\n");
-		printf("안에서 기척이 느껴진다\n");
-		printf("다가가 보시겠습니까?\n");
+		printf("게임을 종료하시겠습니까?");
+		int close = yes_or_no();
 
-		choice = yes_or_no();
-
-		if (choice == 1) {
-			printf("가까이 다가갔더니 인기척이 느껴졌다.\n");
-
+		if (close == 1) {
+			exit(0);
 		}
 
 		else {
-			printf("무엇을 하시겠습니까?\n");
-
-			printf("무언가 수상한 움막을 뒤로한채 발을 옮겼다.\n");
 
 		}
-
-		break;
-	case 5:
-		printf("\n앞에 웬 벙커가 있다.\n");
-		printf("들어가 보시겠습니까?\n");
-
-		choice = yes_or_no();
-
-		if (choice == 1) {
-			printf("가까이 다가갔더니 인기척이 느껴졌다.\n");
-
-		}
-
-		else {
-			printf("무엇을 하시겠습니까?\n");
-
-			printf("무언가 수상한 벙커를 뒤로한채 발을 옮겼다.\n");
-
-		}
-
-		break;
-	case 6:
-		printf("\n앞 길거리에 무언가 있다.\n");
 
 		break;
 	}
 }
 
-void hit_the_road() {
-
-}
-
-void battle() {
-
-}
-
-void display(char p_name[17], int p_HP, int p_stats[5], int p_money) {
+void display(struct main_character *temp) {
 
 	for (int i = 0; i < HEIGHT_GAME; i++) {
 		for (int k = 0; k < WIDTH_GAME; k++) {
@@ -812,63 +1007,52 @@ void display(char p_name[17], int p_HP, int p_stats[5], int p_money) {
 	}
 	
 	gotoxy(188, 1); printf("STATUS");
-	gotoxy(180, 2); printf("Name");
-	gotoxy(200, 2); printf("%s", p_name);
-	gotoxy(180, 4); printf("HP");
-	gotoxy(200, 4); printf("%d", p_HP);
-	gotoxy(180, 5); printf("Strength");
-	gotoxy(200, 5); printf("%d", p_stats[0]);
-	gotoxy(180, 6); printf("Charming");
-	gotoxy(200, 6); printf("%d", p_stats[1]);
-	gotoxy(180, 7); printf("Endurance");
-	gotoxy(200, 7); printf("%d", p_stats[2]);
-	gotoxy(180, 8); printf("Intelligence");
-	gotoxy(200, 8); printf("%d", p_stats[3]);
-	gotoxy(180, 9); printf("Luck");
-	gotoxy(200, 9); printf("%d", p_stats[4]);
-	gotoxy(180, 10); printf("Money");
-	gotoxy(200, 10); printf("%d", p_money);
+	gotoxy(185, 5); printf("Name");
+	gotoxy(195, 5); printf("%s", temp->name);
+	gotoxy(185, 6); printf("Class");
+	gotoxy(195, 6); printf("%s", temp->class);
+	gotoxy(180, 9); printf("HP");
+	gotoxy(195, 9); printf("%d/%d", temp->HP, 50 + temp->LVL * 10 + temp->stats[3] * 5);
+	gotoxy(180, 10); printf("Strength");
+	gotoxy(195, 10); printf("%d", temp->stats[1]);
+	gotoxy(180, 11); printf("Charming");
+	gotoxy(195, 11); printf("%d", temp->stats[2]);
+	gotoxy(180, 12); printf("Endurance");
+	gotoxy(195, 12); printf("%d", temp->stats[3]);
+	gotoxy(180, 13); printf("Intelligence");
+	gotoxy(195, 13); printf("%d", temp->stats[4]);
+	gotoxy(180, 14); printf("Luck");
+	gotoxy(195, 14); printf("%d", temp->stats[5]);
+	gotoxy(180, 15); printf("Money");
+	gotoxy(195, 15); printf("%d", temp->money);
+
 }
 
 int main(void) {
 
 	srand(time(NULL));
 
+	printf("전체화면 플레이를 권장합니다.\n");
+	printf("게임 시작까지\n");
+
+	for (int i = 5; i > 0; i--) {
+		gotoxy(0, 3); printf("%d", i);
+		Sleep(1000);
+	}
+
+	system("cls");
+
 	cell_deployment();
 
-	strcpy_s(player.name, 17, setting_name());
+	struct main_character *player = initialize();
 
-	player.class = setting_class(player.name);
+	while (player->HP > 0) {
 
-	check_N_confirm();
-
-	printf("###초기 스탯을 설정합니다.###\n");
-
-	* player.stats = stat_distribution(player_stats, 10, player.name);
-
-	check_N_confirm();
-
-	player.LVL = 1;
-
-	player.HP = (50 + player.LVL * 10 + player.stats[2] * 5);
-	
-	player.money = 100;
-
-	while (player.HP > 0) {
-
-		display(player.name, player.HP, *player.stats, player.money);
-		hit_the_road();
-		approach();
+		display(player);
+		player_action();
 		system("cls");
 
 	}
-
-	/*
-	chapter1_enter(player_class);
-	chapter_proceed(player_LVL);
-	chapter1_end(player_class);
-	*/
-	
 
 	return 0;
 }
